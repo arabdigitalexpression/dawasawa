e7effd27c5ab1f162cf8e5fb95e2d44369544745
mohamedgundour <mohamedgundour@gmail.com>
2017-02-15 01:03:33 +0200

    preparing for production

--- app/controllers/emailsender.js
+++ app/controllers/emailsender.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var nodemailer = require('nodemailer');
+
 var sendmail = require('sendmail') ({
 	silent: true,
 	dkim: {
@@ -10,38 +10,6 @@ var sendmail = require('sendmail') ({
 
 // load the config file
 var app_config = require('../config/config');
-
-
-var transporter = nodemailer.createTransport({
-	service: "Gmail",
-	auth: {
-		user: app_config.email_address_from,
-		pass: ''
-	}
-});
-
-
-// development sendEmail
-// module.exports.sendEmail = function(email, subject, data) {
-// 	return new Promise(function(resolve, reject) {
-// 		var mailOptions = {
-// 			from: app_config.email_address_from,
-// 			to: email,
-// 			subject: subject,
-// 			html: data.html
-// 		}
-
-// 		transporter.sendMail(mailOptions, function(err, info) {
-// 			if(err) {
-// 				reject(err);
-// 			}
-// 			else {
-// 				resolve();
-// 			}
-// 		})
-// 	});
-// }
-
 
 // production sendEmail
 module.exports.sendEmail = function(email, subject, data) {
@@ -58,33 +26,6 @@ module.exports.sendEmail = function(email, subject, data) {
 		})
 	});
 }
-
-// development receiveEmail
-// module.exports.receiveEmail = function(body) {
-// 	return new Promise(function(resolve, reject) {
-// 		var mailOptions = {
-// 			from: body.name + '&lt;' + body.email + '&gt;' ,
-// 			to: app_config.email_address_writeus,
-// 			subject: 'استمارة الاتّصال',
-// 			html: '<div align="right">'
-// 					+ '<p align="right">بيانات المرسل</p>'
-// 					+ '<p align="right">' + body.user_name + '&lt;' + body.user_email + '&gt;' + '</p>'
-// 					+ '<br>'
-// 					+ '<p align="right">الرسالة</p>'
-// 					+ '<p align="right">' + body.user_message + '</p>'
-// 					+ '</div>'
-// 		}
-
-// 		transporter.sendMail(mailOptions, function(err, info) {
-// 			if(err) {
-// 				reject(err);
-// 			}
-// 			else {
-// 				resolve(info);
-// 			}
-// 		})
-// 	});
-// }
 
 // production receiveEmail
 module.exports.receiveEmail = function(body) {
