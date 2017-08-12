37320b9e59c9952654b371e60e2dd5410a68d22f
mohamedgundour <mohamedgundour@gmail.com>
2017-08-12 20:28:04 +0300

    update captcha  config

--- captcha_server/config/config.js
+++ captcha_server/config/config.js
@@ -1,9 +1,9 @@
 let config = {}
 
 // server config
-config.NAME = "application server"
+config.NAME = "captcha Server"
 config.SITE_URL = "http://localhost"
-config.PORT = 3000
+config.PORT = 4000
 config.VERSION = "1.0.0"
 
 // email service url
@@ -19,10 +19,10 @@ config.DB_NAME = "dawasawa"
 
 
 // email config
-config.EMAIL_FROM = "example@example.com"				// sender email
-config.EMAIL_TO = "example@example.com"				// write us email
-config.EMAIL_USER = "example@example.com"				// email user
-config.EMAIL_PASS = ""							// email password
+config.EMAIL_FROM = "gundourtesting@gmail.com"				// sender email
+config.EMAIL_TO = "gundourtesting@gmail.com"				// write us email
+config.EMAIL_USER = "gundourtesting@gmail.com"				// email user
+config.EMAIL_PASS = "gundour1234"							// email password
 config.EMAIL_SERVICE = "gmail" 								// development email
 config.DKIM_PRIVATE_KEY = "" 								// production email
 config.DKIM_KEY_SELECTOR = ""								// production email
@@ -33,9 +33,8 @@ config.ENCRYPTION_SECRET = "ctIXAq9o3E81JWguImTDajqzk69LmYpUXIcyY3l47Q" // Chang
 
 // application params
 config.INSERTION_CHALLENGE_GRACE = 48		// (hours) the entry removed from the system if not confirmed in 48 hours.
-config.LISTING_CHALLENGE_GRACE = 48 			// (hours) the token link is not accepted after 48 hours
+config.LISTING_CHALLENGE_GRACE = 48			// (hours) the token link is not accepted after 48 hours
 config.EXPIRY_REMOVAL_THRESHOLD = 30			// (days)  entries are removed if it expires in 30 days
-
 
 // captcha params
 config.CAPTCHA_SIZE = "4" 	// nubmer of characters in captcha
